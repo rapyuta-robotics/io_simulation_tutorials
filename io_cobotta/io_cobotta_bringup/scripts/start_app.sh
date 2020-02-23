6ea1e952fed39e340a00bd341e645490a5bc19ab
Yu <yuokamoto1988@gmail.com>
2020-02-23 14:05:14 +0900

    update script

--- io_cobotta/io_cobotta_bringup/scripts/start_app.sh
+++ io_cobotta/io_cobotta_bringup/scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-roslaunch io_motoman_bringup cobotta_app.launch use_rapyuta.io:=true sim:=true
+roslaunch io_cobotta_bringup cobotta_app.launch use_rapyuta.io:=true sim:=true
--- io_cobotta/io_cobotta_bringup/scripts/start_sim.sh
+++ io_cobotta/io_cobotta_bringup/scripts/start_sim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-roslaunch io_motoman_bringup cobotta_sim.launch use_rapyuta.io:=true
+roslaunch io_cobotta_bringup cobotta_sim.launch use_rapyuta.io:=true
